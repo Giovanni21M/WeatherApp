73031da37c683d25e72c591e2f6e6a3756280492
Giovanni21M <giovanni21martinus@gmail.com>
2019-01-30 08:34:33 -0500

    Create DownloadTask.doInBackground() to connect to browser with given url and scrape web content

--- app/src/main/java/com/giovannimartinus/weatherapp/MainActivity.java
+++ app/src/main/java/com/giovannimartinus/weatherapp/MainActivity.java
@@ -1,9 +1,53 @@
 package com.giovannimartinus.weatherapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection;
+
+            try {
+                // connect to the browser with the given url
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+
+                // download the input stream
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader streamReader = new InputStreamReader(inputStream);
+
+                int data = streamReader.read();
+
+                // add scraped web content to result
+                while (data != -1) {
+                    char currentChar = (char) data;
+                    result += currentChar;
+                    data = streamReader.read();
+                }
+
+                return result;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
